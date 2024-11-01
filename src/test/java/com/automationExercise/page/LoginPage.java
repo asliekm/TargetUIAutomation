6a7af6507453dc745544ac1f962505612a2d8811
uguryamaan <ugur.yamaan@gmail.com>
2024-11-01 03:37:02 -0700

    Login features are created and files from the last project were deleted

--- src/test/java/com/automationExercise/page/LoginPage.java
+++ src/test/java/com/automationExercise/page/LoginPage.java
@@ -5,25 +5,45 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class LoginPage {
 
-        public LoginPage() {
 
-                PageFactory.initElements(Driver.getDriver(), this);
-        }
-        @FindBy(xpath = "/html/body/section/div/div/div[1]/div[1]/h2")
-        public WebElement loginToYouraccount;
-        @FindBy(xpath="//input[@data-qa='login-email']")
-        public WebElement emailField;
-        @FindBy(xpath="//input[@data-qa='login-password']")
-        public WebElement passwordField;
-        @FindBy(xpath="//button[@data-qa='login-button']")
-        public WebElement signlogin;
-        @FindBy(xpath="  //ul[@class='nav navbar-nav']//li[10]")
-        public WebElement loggedName;
+    public LoginPage(){
 
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 
+    @FindBy(xpath = "(//input[@name='searchTerm'])[1]")
+    public WebElement homePageSearchbox;
+
+    @FindBy(xpath = "//*[@class='h-margin-r-x1']")
+    public List<WebElement> bulunanUrunElementleriList;
+
+    @FindBy(xpath = "//*[@class='sc-58ad44c0-3 kwbrXj h-margin-r-x3']")
+    public WebElement homePageSigninButton;
+
+    @FindBy(xpath = "//*[@class='sc-859e7637-0 hHZPQy']")
+    public WebElement barSignInButton;
+
+    @FindBy(xpath = "//*[@id='username']")
+    public WebElement emailField;
+
+    @FindBy(xpath = "//*[@id='password']")
+    public WebElement passwordField;
+
+    @FindBy(xpath = "//*[@*='sc-ddc722c0-0 sc-f1230b39-0 sc-ea08e237-4 bsiKgd doBYzz bveVWO']")
+    public WebElement signInWithPasswordButton;
+
+    @FindBy(xpath = "//span[text()='Verification code sent']")
+    public WebElement verificationCodeSentScript;
+
+    @FindBy(xpath = "(//*[@*='sc-859e7637-0 hHZPQy'])[2]")
+    public WebElement createAccountButton;
+
+    @FindBy(xpath = "//*[@*='sc-f86dede5-4 gseCQf h-bg-grayLightest']")
+    public WebElement birthdaySaleScript;
 
 
 }
-
--- src/test/java/com/automationExercise/runners/CukesRunner.java
+++ src/test/java/com/automationExercise/runners/CukesRunner.java
@@ -9,8 +9,8 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features", // Path to your feature files
         glue = "com.automationExercise.steps", // Package with your step definitions
         plugin = {"pretty", "html:target/cucumber-reports.html"}, // Reporting options
-        monochrome = true ,// Enables readable output in console
-        tags= "@loginTarget"
+        monochrome = false,// Enables readable output in console
+        tags= "@wip"
 )
 public class CukesRunner {
     // This class will remain empty. It is used as a glue between Cucumber and JUnit.
